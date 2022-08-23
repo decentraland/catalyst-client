22248f778bf7983bfcd00882881d2fadc1e37e6c
Agustina Aldasoro <agusaldasoro@gmail.com>
2022-08-23 11:35:13 -0300

    Fail when no id is sent while getting profiles

--- src/LambdasClient.ts
+++ src/LambdasClient.ts
@@ -36,6 +36,9 @@ export class LambdasClient implements LambdasAPI {
   }
 
   fetchProfiles(ethAddresses: string[], profileOptions?: ProfileOptions, options?: RequestOptions): Promise<any[]> {
+    if (ethAddresses.length === 0) {
+      throw new Error('At least one id must be present when requesting profiles')
+    }
     const queryParams: Map<string, string[]> = new Map()
     queryParams.set('id', ethAddresses)
     if (profileOptions?.fields) {
