d96e9291f38d2a93663fcbcc0c4e4346209bbaca
Mariano Goldman <mariano.goldman@decentraland.org>
2024-02-07 13:41:36 -0300

    feat: add method buildEntityWithoutNewFiles

--- src/client/utils/index.ts
+++ src/client/utils/index.ts
@@ -1,3 +1,3 @@
-import { buildEntity, buildEntityAndFile, DeploymentData } from './DeploymentBuilder'
-export const DeploymentBuilder = { buildEntity, buildEntityAndFile }
+import { buildEntity, buildEntityAndFile, buildEntityWithoutNewFiles, DeploymentData } from './DeploymentBuilder'
+export const DeploymentBuilder = { buildEntity, buildEntityAndFile, buildEntityWithoutNewFiles }
 export { DeploymentData }
