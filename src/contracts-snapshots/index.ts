6d287bdf051db3b207b4c6872e0dc9726cfe3429
Gabriel Díaz <git@subsme.slmail.me>
2024-04-16 17:22:17 -0300

    feat: Add polygon amoy support (#455)
    * feat: Use polygon amoy contract
    
    * feat: Update @dcl/catalyst-contracts package
    
    * chore: Upgrade @dcl/catalyst-contracts and @dcl/schemas package

--- src/contracts-snapshots/index.ts
+++ src/contracts-snapshots/index.ts
@@ -1,5 +1,5 @@
-import { cache } from './data'
 import { CatalystServerInfo, L1Network } from '@dcl/catalyst-contracts'
+import { cache } from './data'
 
 export function getCatalystServersFromCache(network: L1Network): CatalystServerInfo[] {
   return cache.catalysts[network]
@@ -9,6 +9,6 @@ export function getNameDenylistFromCache(network: 'mainnet'): string[] {
   return cache.nameDenylist[network]
 }
 
-export function getPoisFromCache(network: 'polygon' | 'mumbai'): string[] {
+export function getPoisFromCache(network: 'polygon' | 'amoy'): string[] {
   return cache.pois[network]
 }
